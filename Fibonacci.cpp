f243502b116a9ef30742d8c748f09c77ef463da2
juanmvj <juanmvj_1996@live.com.mx>
2016-09-12 20:46:00 -0500

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+unsigned int Fibonacci(unsigned int n)
+{
+	unsigned int a = 0, b = 1, c;
+	if (n < 2)
+	{
+		return n;
+	}
+	for (int i = 1; i < n; ++i)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+
+unsigned int FibonacciR(unsigned int n)
+{
+	if (n < 2)
+	{
+		return n;
+	}
+
+	return FibonacciR(n - 2) + FibonacciR(n - 1);
+}
+
+int main()
+{
+	printf("%u\n",Fibonacci(10000000000000000000));
+	printf("%u\n",FibonacciR(51));
+
+
+	return 0;
+}
